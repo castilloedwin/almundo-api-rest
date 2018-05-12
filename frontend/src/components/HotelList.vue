c7c25e8aae025c5b6fdd47c65d46dca8dc660a4f
Edwin Castillo <eocc28@hotmail.com>
2018-05-11 22:09:03 -0500

    Mostrando el listado de hoteles por medio de la API

--- frontend/src/components/HotelList.vue
+++ frontend/src/components/HotelList.vue
@@ -1,22 +1,55 @@
 <template>
 	<div>
-		<div class="info-hotel">
-			<div class="picture-hotel">
-				<img src="/static/assets/images/hotels/2a5bb415_b.jpg" alt="" />
+		<div class="info-hotel" v-for="hotel in hotels">
+			<div class="column-left">
+				<div class="picture-hotel">
+					<img :src="'/static/assets/images/hotels/' + hotel.image" alt="" />
+				</div>
+				<div class="name-stars-hotel">
+					<p v-text="hotel.name"></p>
+					<figure class="stars">
+						<img v-for="stars in hotel.stars" width="20" src="/static/assets/icons/filters/star.svg" alt="" />
+					</figure>
+					<div class="amenities">
+						<span v-for="amenitie in hotel.amenities">
+							<img width="18" :src="'/static/assets/icons/amenities/' + amenitie + '.svg'" alt="" />
+						</span>
+					</div>
+				</div>
 			</div>
-			<div class="name-stars-hotel">
-				<p>Hotel Stefanos</p>
-				<figure class="stars">
-					<img width="20" src="/static/assets/icons/filters/star.svg" alt="">
-				</figure>
+			<div class="price-hotel">
+				<p class="small-text">Precio por noche por habitación</p>
+				<p class="price"><span>ARS</span> {{ hotel.price }}</p>
+				<a class="see-hotel" href="">Ver hotel</a>
 			</div>
-			<div class="price-hotel"></div>
 		</div>
 	</div>
 </template>
 
 <script>
 	
+	import axios from 'axios';
+
+	export default {
+
+		created() {
+			this.getHotels();	
+		},
+		data() {
+			return {
+				hotels: []
+			}
+		},
+		methods: {
+			getHotels: function () {
+				axios.get('http://localhost:8000/api/v1/hotels').then(response => {
+					this.hotels = response.data;
+				});
+			}
+		}
+
+	}
+
 </script>
 
 <style lang="sass" scoped>
@@ -32,27 +65,79 @@
 		background-color: white
 		border: solid 1px darken($gray, 7%)
 
-		.picture-hotel
-			margin-right: 10px
+		.column-left
 
-	.name-stars-hotel
-		p
-			margin-bottom: 5px
-			color: $blueText
+			.picture-hotel
+				height: 190px
+				margin-right: 15px
+				overflow: hidden
 
-		.stars
-			display: inline-block
-			vertical-align: center
+				img
+					width: 290px
 
-	.price-hotel
+		.name-stars-hotel
+			p
+				margin-bottom: 5px
+				color: $blueText
+
+			.stars
+				margin-bottom: 10px
+				display: inline-block
+				vertical-align: center
+
+			.amenities
+				span
+					&:not(:last-child)
+						margin-right: 10px
+
+		.price-hotel
+			padding: 34px 20px
+			text-align: center
+			border-left: dotted 2px #CCC
+
+
+			.small-text
+				width: 170px
+				margin-bottom: 10px
+				font-size: 0.8em
+				color: #929292
+
+			.price
+				margin-bottom: 13px
+				font-size: 2em
+				font-weight: bold
+				color: $orange
+
+				span
+					font-size: 0.7em
+					font-weight: 200
+
+			.see-hotel
+				padding: 7px 25px
+				background-color: $blue
+				display: block
+				color: #FFF
+				font-size: 1.2em
+				font-weight: 300
+				text-align: center
+				text-decoration: none
+				border-radius: 5px
+
+				&:hover
+					background-color: darken($blue, 7%)
 
 	@media screen and(min-width: 1024px)
 		.info-hotel
 			display: flex
 			flex-wrap: wrap
 			align-items: start
+			justify-content: space-between
 
-			.picture-hotel
+			.column-left
+				display: flex
+				flex-wrap: wrap
+
+				.picture-hotel
 
 		.name-stars-hotel
 
